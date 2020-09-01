72238ede90ee1d8c89a647668db56ceaa74b8b56
Harry LB <harry.lingardbright@gmail.com>
2020-09-01 16:25:48 +0100

    can delete wallposts

--- app/controllers/wallposts_controller.rb
+++ app/controllers/wallposts_controller.rb
@@ -1,14 +1,17 @@
 class WallpostsController < ApplicationController
   before_action :logged_in_user
   
-  def new
-    @user = User.find(params[:user_id])
-    @wallpost = Wallpost.new
-  end
-
   def create
     @user = User.find(params[:user_id])
     @wallpost = @user.wallposts.create(wallpost_params.merge(poster: current_user.name))
+
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @wallpost = @user.wallposts.find(params[:id])
+    @wallpost.destroy
 
     redirect_to user_path(@user)
   end
--- spec/features/wallposts_spec.rb
+++ spec/features/wallposts_spec.rb
@@ -10,4 +10,15 @@ RSpec.feature 'Wall Posts' do
     click_button 'Submit'
     expect(page).to have_content 'Player 2 Hi, Test!'
   end
+  scenario 'poster can delete their post' do
+    sign_up 
+    click_link('Log out')
+    sign_up_another_account
+    visit '/users/1'
+    expect(page).to have_content 'Add a wallpost:'
+    fill_in 'wallpost[content]', with: 'Hi, Test!'
+    click_button 'Submit'
+    click_link 'Delete'
+    expect(page).to_not have_content 'Player 2 Hi, Test!'
+  end
 end
