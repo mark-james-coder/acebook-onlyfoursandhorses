7e06c91c66365b4a113c3dbe7c216981f8f6e46c
Mark <markjpoole@yahoo.com>
2020-09-03 18:02:55 +0100

    navigation changes

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -1,10 +1,12 @@
 feature 'deleting users' do
   scenario 'there is a delete user button' do
     sign_up
+    click_link 'Account'
     expect(page).to have_link 'Delete Account'
   end
   scenario 'pressing it deletes user and redirects to homepage' do
     sign_up
+    click_link 'Account'
     click_link 'Delete Account'
     expect(current_path).to eq '/'
   end
--- spec/features/edit_users_spec.rb
+++ spec/features/edit_users_spec.rb
@@ -1,18 +1,18 @@
 feature 'editing users' do
   scenario 'there is an edit users button' do
     sign_up
-    expect(page).to have_link 'Edit Account'
+    expect(page).to have_link 'Account'
   end
   scenario 'takes us to a new form' do
     sign_up
-    click_link 'Edit Account'
+    click_link 'Account'
     expect(page).to have_field('user[name]')
     expect(page).to have_field('user[email]')
     expect(page).to have_field('user[password]')
   end
   scenario 'editing the user edits the user' do
     sign_up
-    click_link 'Edit Account'
+    click_link 'Account'
     fill_in 'user[name]', with: 'Edit Johnson'
     fill_in 'user[email]', with: 'Editjohnson@testmail.com'
     fill_in 'user[password]', with: '123456'
@@ -21,7 +21,7 @@ feature 'editing users' do
   end
   scenario 'trying to click update button without filling forms' do
     sign_up
-    click_link 'Edit Account'
+    click_link 'Account'
     click_button 'Update User'
     expect(page).to have_field('user[name]')
     expect(page).to have_field('user[email]')
