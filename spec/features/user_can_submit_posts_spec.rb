93fc7a025f6562391e80bfef9382deb750b5a5bc
Harry LB <harry.lingardbright@gmail.com>
2020-09-04 15:22:37 +0100

    individual page layout changes, buttons etc

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'posting posts', type: :feature do
   scenario 'Can submit posts and view them' do
     sign_up
-    click_link 'See Posts'
+    click_link 'Posts'
     click_link 'New post'
     fill_in 'post[message]', with: 'Hello, world!'
     click_button 'Submit'
--- spec/features/user_can_update_posts_spec.rb
+++ spec/features/user_can_update_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Posts', type: :feature do
   scenario 'updating posts' do
     sign_up
-    click_link 'See Posts'
+    click_link 'Posts'
     click_link 'New post'
     fill_in 'post[message]', with: 'Testing'
     click_button 'Submit'
