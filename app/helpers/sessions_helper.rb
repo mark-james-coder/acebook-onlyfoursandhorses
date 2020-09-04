dafecd1f8eef6ef4e08e0eeb9b8c5167e3b68dd7
Harry LB <harry.lingardbright@gmail.com>
2020-09-04 12:32:57 +0100

    started layouts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
+  def current_user 
     return unless session[:user_id]
 
     @current_user ||= User.find_by(id: session[:user_id])
